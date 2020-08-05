baa0151e94d4959d4352e4c77a4786176ff8049d
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-05 13:24:12 +0530

    storing value in directories staticaly

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash  
 read -p " plz enter value FOR B:" b
 read -p " plz enter value FOR C:" c
 read -p " plz enter value FOR A:" a
@@ -6,3 +6,9 @@ ans1=$(( ($a+$b)*$c ))
 ans2=$(( ($a%$b)+$c ))
 ans3=$(( ($a+$c)/$b ))
 ans4=$(( ($a*$b)+$c ))
+declare -A answer
+answer[0]=$ans1
+answer[1]=$ans2
+answer[2]=$ans3
+answer[3]=$ans4
+echo ${answer[@]}
